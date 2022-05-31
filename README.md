0bb25d270acedb4d4466d9b2bc2a2c5fd6e5531b
Hussam Hussain <hussamhussain11@gmail.com>
2022-05-31 18:32:51 +0100

    Reedone using Odins way instead. This one includes a function that plays thee game 5 times and returns whether you won or lost

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rock-paper-scissors
 
 This is the first iteration of rock, paper, scissors which has no GUI. Everything is in the console for now.
+
+Redoing it in odins way which includes functions instead. 
--- script.js
+++ script.js
@@ -1,36 +1,85 @@
-let rps = ['rock','paper','scissors'];
+function computerPlay() {
+  
+  let rps = ['rock','paper','scissors'];
+  let random = Math.floor(Math.random()*3);
+  return rps[random];
 
-let UserChoice = prompt("Choose between rock, paper and scissors.");
-UserChoice = UserChoice.toLowerCase();
-
-if (UserChoice != 'rock' && UserChoice != 'paper' && UserChoice != 'scissors') {
-  console.log("Type in either rock, paper or scissors.");
 }
 
-else {
+function playRound(playerSelection, computerSelection) {
+
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+    return "Type in either rock, paper or scissors.";
+  }
   
-  let random = Math.floor(Math.random()*3);
-  let CompChoice = rps[random];
+  else {
+    
+    switch (true) {
   
-  switch (true) {
-
-    case (UserChoice == CompChoice):
-      console.log(`You chose ${UserChoice} whilst computer chose ${CompChoice}...it is a tie!`);
-      break;
-
-    case (UserChoice == 'rock' && CompChoice == 'paper'):
-    case (UserChoice == 'paper' && CompChoice == 'scissors'):
-    case (UserChoice == 'scissors' && CompChoice == 'rock'):
-      console.log(`You chose ${UserChoice} whilst computer chose ${CompChoice}...you lost!`);
-      break;
-
-    case (UserChoice == 'rock' && CompChoice == 'scissors'):
-    case (UserChoice == 'paper' && CompChoice == 'rock'):
-    case (UserChoice == 'scissors' && CompChoice == 'paper'):
-      console.log(`You chose ${UserChoice} whilst computer chose ${CompChoice}...you won!`);
-      break;
-
-    default:
-      console.log("Somethings gone terribly wrong, oh dear...");
+      case (playerSelection == computerSelection):
+        return `You chose ${playerSelection} whilst computer chose ${computerSelection}...it is a tie!`;
+        break;
+  
+      case (playerSelection == 'rock' && computerSelection == 'paper'):
+      case (playerSelection == 'paper' && computerSelection == 'scissors'):
+      case (playerSelection == 'scissors' && computerSelection == 'rock'):
+        return `You chose ${playerSelection} whilst computer chose ${computerSelection}...you lost!`;
+        break;
+  
+      case (playerSelection == 'rock' && computerSelection == 'scissors'):
+      case (playerSelection == 'paper' && computerSelection == 'rock'):
+      case (playerSelection == 'scissors' && computerSelection == 'paper'):
+        return `You chose ${playerSelection} whilst computer chose ${computerSelection}...you won!`;
+        break;
+  
+      default:
+        return "Somethings gone terribly wrong, oh dear...";
+    }
   }
 }
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (i=0; i<5; i++) {
+
+    playerSelection = prompt('choose between rock, paper and scissors');
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+      i--;
+    }
+    computerSelection = computerPlay()
+    console.log(playRound(playerSelection,computerSelection));
+    
+    switch (true) {
+      case (playerSelection == computerSelection):
+        playerScore +=1;
+        computerScore +=1;
+        break;
+
+      case (playerSelection == 'rock' && computerSelection == 'paper'):
+      case (playerSelection == 'paper' && computerSelection == 'scissors'):
+      case (playerSelection == 'scissors' && computerSelection == 'rock'):
+        computerScore +=1;
+        break;
+
+      case (playerSelection == 'rock' && computerSelection == 'scissors'):
+      case (playerSelection == 'paper' && computerSelection == 'rock'):
+      case (playerSelection == 'scissors' && computerSelection == 'paper'):
+        playerScore +=1;
+        break;
+
+      default:
+        break;
+    }
+  
+  
+  }
+ 
+  if (playerScore == computerScore) return "You tied!";
+  else if (playerScore > computerScore) return `You won`;
+  else return 'You lost';
+}
+
