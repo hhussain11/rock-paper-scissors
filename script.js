996b1db9a79c9175698b26dded13d557b61ebb4e
Hussam Hussain <hussamhussain11@gmail.com>
2022-07-20 16:01:04 +0100

    Fully working with a basic UI

--- script.js
+++ script.js
@@ -7,52 +7,35 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
-  playerSelection = playerSelection.toLowerCase();
-
-  if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-    return "Type in either rock, paper or scissors.";
-  }
   
-  else {
-    
-    switch (true) {
+  switch (true) {
   
-      case (playerSelection == computerSelection):
-        return `You chose ${playerSelection} whilst computer chose ${computerSelection}...it is a tie!`;
-        break;
+    case (playerSelection == computerSelection):
+      return `You chose ${playerSelection} whilst computer chose ${computerSelection}...it is a tie!`;
+      break;
   
-      case (playerSelection == 'rock' && computerSelection == 'paper'):
-      case (playerSelection == 'paper' && computerSelection == 'scissors'):
-      case (playerSelection == 'scissors' && computerSelection == 'rock'):
-        return `You chose ${playerSelection} whilst computer chose ${computerSelection}...you lost!`;
-        break; 
+    case (playerSelection == 'rock' && computerSelection == 'paper'):
+    case (playerSelection == 'paper' && computerSelection == 'scissors'):
+    case (playerSelection == 'scissors' && computerSelection == 'rock'):
+      return `You chose ${playerSelection} whilst computer chose ${computerSelection}...you lost!`;
+      break; 
   
-      default:
+    default:
         
-        return `You chose ${playerSelection} whilst computer chose ${computerSelection}...you won!`;
+      return `You chose ${playerSelection} whilst computer chose ${computerSelection}...you won!`;
     }
-  }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  play: for (i=0; i<5; i++) {
+function game(playerSelection) {
 
-    playerSelection = prompt('choose between rock, paper and scissors');
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-      i--;
-      continue play;
-    }
-    computerSelection = computerPlay()
-    console.log(playRound(playerSelection,computerSelection));
     
+    computerSelection = computerPlay();
+    result.textContent = playRound(playerSelection,computerSelection);
+
+
     switch (true) {
       case (playerSelection == computerSelection):
-        playerScore +=1;
-        computerScore +=1;
+       
         break;
 
       case (playerSelection == 'rock' && computerSelection == 'paper'):
@@ -63,14 +46,43 @@ function game() {
 
       default:
         playerScore +=1;
-        
     }
-  console.log(`Player: ${playerScore}`);
-  console.log(`Computer: ${computerScore}` );
-  
-  }
- 
-  if (playerScore == computerScore) return "You tied!";
-  else if (playerScore > computerScore) return `You won`;
-  else return 'You lost';
+
+    playerDiv.textContent = `Player Score: ${playerScore}`;
+    computerDiv.textContent = `Computer Score: ${computerScore}`;
+
+    if (computerScore == 5) {
+      
+      result.textContent = "Unlucky! You lost the game!";
+      playerScore = 0;
+      computerScore = 0;
+
+    }
+
+    if (playerScore == 5) {
+
+      result.textContent = "Congratulations! You won the game!";
+      playerScore = 0;
+      computerScore = 0;
+
+    }
+
+
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+let rockButton = document.querySelector(".rock");
+let paperButton = document.querySelector(".paper");
+let scissorsButton = document.querySelector(".scissors");
+let playerDiv = document.querySelector(".player")
+let computerDiv = document.querySelector(".computer")
+let result = document.querySelector(".result");
+
+
+
+rockButton.addEventListener('click', () => game('rock'))
+paperButton.addEventListener('click', () => game('paper'))
+scissorsButton.addEventListener('click', () => game('scissors'))
+
